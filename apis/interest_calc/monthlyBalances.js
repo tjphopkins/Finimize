1f6a46a046784b61d0d0eb59ecc121d19aeca483
tjphopkins <tomjphopkins@gmail.com>
2017-07-22 14:19:53 +0100

    getMonthlyBalances method

---
+++ apis/interest_calc/monthlyBalances.js
@@ -0,0 +1,49 @@
+const assert = require('assert');
+
+
+let oneMonthInvestment = function(
+        balance,  periodsPerYear, annualInterest,
+        monthlyDeposit, monthsElapsed) {
+    assert([12, 4, 1].includes(periodsPerYear));
+    const modulo = 12 / periodsPerYear;
+    if ((monthsElapsed - 1) % modulo === 0) {
+        return (
+            balance * (
+                (1 + (annualInterest / periodsPerYear))) + monthlyDeposit
+        );
+    }
+    return (balance + monthlyDeposit);
+}
+
+
+let getMonthlyBalances = function(
+        initialBalance, periodsPerYear, annualInterest,
+        monthlyDeposit, investedMonths, monthlyBalances=[], monthsElapsed=0) {
+    /*
+    :arg initialBalance Number
+    :arg periodsPerYear Number - Number of compounding periods per year
+    :arg annualInterest Number
+    :arg monthlyDeposit Number
+    :arg investedMonths Number - Number of months to invest for
+    :kwarg monthlyBalances Array, default []
+    :kwarg monthsElapsed Number, default 0
+
+    :return monthlyBalances Array - where the ith element is the balance in
+                                    month i
+    */
+    if (monthsElapsed === investedMonths) {
+        return monthlyBalances;
+    }
+    let newBalance = oneMonthInvestment(
+        initialBalance, periodsPerYear, annualInterest,
+        monthlyDeposit, monthsElapsed
+    );
+    monthsElapsed += 1;
+    monthlyBalances.push(Number(initialBalance).toFixed(2) / 1);
+    return getMonthlyBalances(
+        newBalance, periodsPerYear, annualInterest,
+        monthlyDeposit, investedMonths, monthlyBalances, monthsElapsed);
+}
+
+
+module.exports = getMonthlyBalances;
